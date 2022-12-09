30dc52634f36be196dcc15379664556264f3138f
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-08 16:13:57 -0800

    msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+
 int main(void)
 {
-	char STR[] ="\"Programming is like building a multilingual puzzle";
-	puts(STR);
-	return(0);
+	char STR[] = "\"Programming is like building a multilingual puzzle";
+		puts(STR);
+		return (0);
 }
