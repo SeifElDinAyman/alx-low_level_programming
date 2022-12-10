4c20e50e4de21321955f40d0a2e92a2ed4f6585f
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-09 16:41:27 -0800

    msg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+/* comment*/
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
