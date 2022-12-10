1acec3b9c05965b31c3f85e1058cab8f6f6fa55a
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-09 16:38:56 -0800

    msg

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+/* comment*/
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
