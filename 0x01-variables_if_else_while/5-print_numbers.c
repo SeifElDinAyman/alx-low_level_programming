0ad45211d5a545d2af3f00a9c50d9a4655baabeb
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-09 16:29:19 -0800

    msg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+/* comment*/
+	for (ch = 0; ch <= 9; ch++)
+	{
+		printf("%d", ch);
+	}
+	printf("\n");
+	return (0);
+
+}
