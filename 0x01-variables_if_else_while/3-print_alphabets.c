5e071f67ca589d675010006f092b4350009bfff5
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-09 16:18:14 -0800

    msg

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+/* comment*/
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+}
