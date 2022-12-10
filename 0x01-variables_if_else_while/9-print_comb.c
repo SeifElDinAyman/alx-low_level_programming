64dc7c74feb724ef897887bd6f57382554a1af30
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-09 16:50:07 -0800

    msg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+/* comment*/
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+
+}
