b94c33056ae620347881fedb00ac53f1270cd73a
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-13 15:38:06 -0800

    msg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc */
+/**
+ * main - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
