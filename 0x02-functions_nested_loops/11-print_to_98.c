72daac5172544a16a34d1e3c6752b585c5add699
Seif El-Din Ayman <seifeldinayman77.com>
2022-12-14 04:33:30 +0200

    msg

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc */
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: is the char to be checked
+ *  Return: Always 0 (success)
+ */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+			}
+		}
+
+}
