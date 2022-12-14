6700821a06e4902e16a9668d4660bdf81a1219b9
Seif El-Din Ayman <seifeldinayman77.com>
2022-12-14 02:56:20 +0200

    msg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc */
+
+/**
+ *  print_alphabeit_x10 - Entry point
+ *
+ *
+ *  Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+
+	char letter;
+	int count = 0 ;
+	/* desc */
+	while (count <= 10){
+		
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		count++;
+	}
+	_putchar('\n');
+}
