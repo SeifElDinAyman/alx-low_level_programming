7a0b9c66d4d61e00da46d3dfd4b2c099deacba78
Seif El-Din Ayman <seifeldinayman77.com>
2022-12-14 04:37:40 +0200

    msg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc */
+
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+int main(void)
+{
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
