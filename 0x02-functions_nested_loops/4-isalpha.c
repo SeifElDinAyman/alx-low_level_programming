8a12403c0215008439a3e9ed2f80bd13fc1b9d58
Seif El-Din Ayman <seifeldinayman77.com>
2022-12-14 03:26:02 +0200

    msg

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc */
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: is the char to be checked
+ *  Return: Always 0 (success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+
+}
