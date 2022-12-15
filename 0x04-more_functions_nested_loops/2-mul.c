dc6e9671a7c69e01dc7a4ea93e32e2bb2ad91212
SeifElDinAyman <seifeldinayman77@gmail.com>
2022-12-15 16:20:52 +0200

    msg

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* int mul- checks for uppercase character
+* @a: Variable text
+* @b: Num2
+* Return: Always 0.
+*/
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
