179532fafb507f7423996022370fe400206c5822
Seif El-Din Ayman <seifeldinayman77.com>
2022-12-15 11:26:56 +0200

    msg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.c"
+#include <stdio.h>
+/*
+ * postive_or_negative - Function
+ *
+ */
+void positive_or_negative(int i);
+{
+	int i
+		/* comment */
+		i = 98;
+	postive_or_negative(i);
+	return (0);
+}
