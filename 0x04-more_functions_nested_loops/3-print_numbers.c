994ebe8f9be46b57215f8136d20b9575bec72286
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-15 06:38:09 -0800

    msg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbes - checks for uppercase character
+* @a: Variable text
+* @b: Num2
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for(num = 10; num < 19; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
