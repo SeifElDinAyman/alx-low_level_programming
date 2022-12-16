5852cc3cc5d5a12218dfb8a438961695a4b1f650
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-16 03:13:54 +0200

    msg

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,8 @@ if (n < 0)
 {
 n1 = -n;
 _putchar('-');
-} else
+}
+else
 {
 n1 = n;
 }
@@ -18,6 +19,5 @@ if (n1 / 10)
 {
 print_number(n1 / 10);
 }
-
- _putchar((n1 % 10) + '0');
- }
+_putchar((n1 % 10) + '0');
+}
