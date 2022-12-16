308de27a11f90336af425a72fe7788f4f80fffc4
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-15 16:15:16 -0800

    msg

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
-#include "main.h"
+
+
+
 /**
  *
- * * print_remaining_days - takes a date and prints how many days are
+ * * main - causes an infinite loop
  *
- * * left in the year, taking leap years into account
- *
- * * @month: month in number format
- *
- * * @day: day of month
- *
- * * @year: year
- *
- * * Return: void
+ * * Return: 0
  *
  * */
 
-void print_remaining_days(int month, int day, int year)
+
+
+int main(void)
+
 {
-if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
-{
-		if (month >= 3 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+
+		int i;
+
+
+
+			printf("Infinite loop incoming :(\n");
+
+				/**
+				 *
+				 * 	* i = 0;
+				 *
+				 * 		*
+				 *
+				 * 			* while (i < 10)
+				 *
+				 * 				* {
+				 *
+				 * 					* putchar(i);
+				 *
+				 * 						* }
+				 *
+				 * 							*/
+
+				printf("Infinite loop avoided! \\o/\n");
+
+
+
+					return (0);
+
 }
