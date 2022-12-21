605a37022dffe9697da1c5efdb7aa38ef88cd41e
Seif El-Din <SeifElDinAyman77@gmail.com>
2022-12-21 17:18:49 +0200

    msg

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - string
+ * @dest:  value
+ * @src:  value
+ * @n:  value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int y;
+
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+		dest[y] = src[y];
+		y++;
+	}
+	while (y < n)
+	{
+		dest[y] = '\0';
+		y++;
+	}
+
+	return (dest);
+}
