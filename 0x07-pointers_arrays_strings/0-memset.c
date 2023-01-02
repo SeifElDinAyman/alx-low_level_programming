f41a5eed67ab62258fde1d465e4378e991efde01
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-01-03 00:11:11 +0200

    msg

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,23 @@
-#include "main.h"
+#include"main.h"
 /**
- * _memset - is a function
- * @s: array
- * @b: constant
- * @n: fill value
+ * _memset - function
  *
- * Return: changed array with new value for n bytes
+ * @s:pointer
+ * @b: constant
+ * @n: counter 
+ *
+ * Return: (s)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int count = 0;
+	unsigned int counter;
 
-	for (; n > 0; count++)
+	for(counter = 0; counter < n; counter++ )
 	{
-		s[count] = b;
-		n--;
+		s[counter] = b;
 	}
-	return (s);
-}
 
+
+
+	return(s);
+}
