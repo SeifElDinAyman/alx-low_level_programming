ecfb4f685788ebc90f38034bd40c4cc28a4036b2
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-01-12 12:43:18 +0200

    msg

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,10 +5,10 @@
  * @b: value
  * Return: ptr
  */
-void *malloc_checked(unsigned int b)	
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
-	
+
 	ptr = malloc(b);
 	if (ptr == NULL)
 	exit(98);
