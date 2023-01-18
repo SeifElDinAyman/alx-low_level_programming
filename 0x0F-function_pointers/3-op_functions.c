31b5f0f52c4414a47901ca1e9f7f68549e337d29
Seif El-Din <SeifElDinAyman77@gmail.com>
2023-01-18 17:10:28 +0200

    msg

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,24 @@
+#include"3-calc.h"
+#include<stdlib.h>
+/**
+* get_op_func - fn
+ * @s: sign
+ *
+ * Return: ptr.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int num = 0;
+
+	while (ops[num].op != NULL && *(ops[num].op) != *s)
+		num++;
+	return (ops[num].f);
+}
